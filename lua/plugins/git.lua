3d86db35500493e24f3d8d0389b1f4bb38dd5be1
Mike Joyce <mike.joyce@shipt.com>
2024-02-23 05:54:00 -0500

    formatting?

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,16 +1,16 @@
 return {
-	-- lets see what git-blame and gitsigns give me
-	-- {
-	-- 	"tpope/vim-fugitive",
-	-- },
-	{
-		"f-person/git-blame.nvim",
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-		end,
-	},
+  -- lets see what git-blame and gitsigns give me
+  -- {
+  -- 	"tpope/vim-fugitive",
+  -- },
+  {
+    "f-person/git-blame.nvim",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    end,
+  },
 }
