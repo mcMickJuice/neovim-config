8771406ac5f7c20dc8de585361027b291eeeaf5c
Mike Joyce <mike.joyce@shipt.com>
2024-02-23 06:33:43 -0500

    add back fugitive, add rhubarb

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,16 +1,16 @@
 return {
-  -- lets see what git-blame and gitsigns give me
-  -- {
-  -- 	"tpope/vim-fugitive",
-  -- },
-  {
-    "f-person/git-blame.nvim",
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    end,
-  },
+	{
+		"tpope/vim-fugitive",
+	},
+	{ "tpope/vim-rhubarb" },
+	{
+		"f-person/git-blame.nvim",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
 }
