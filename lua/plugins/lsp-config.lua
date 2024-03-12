58fb2e7e747018875f1256eaaf0d16c3b0b203f2
Mike Joyce <mike.joyce@shipt.com>
2024-03-12 19:29:20 -0400

    update lsp-config?

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,52 +1,52 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "gopls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "gopls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-			--    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-			--    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-			--    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-			--    vim.keymap.set('n', '<leader>wl', function()
-			--      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			--    end, opts)
-			--    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-			--    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-			--    vim.keymap.set('n', '<leader>f', function()
-			--     vim.lsp.buf.format { async = true }
-			--  end, {})
-		end,
-	},
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      --    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+      --    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+      --    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+      --    vim.keymap.set('n', '<leader>wl', function()
+      --      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      --    end, opts)
+      --    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+      --    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+      --    vim.keymap.set('n', '<leader>f', function()
+      --     vim.lsp.buf.format { async = true }
+      --  end, {})
+    end,
+  },
 }
