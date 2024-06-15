4ef8256c84affabc0600bd4ab38e0a041e6b7eb0
Mike Joyce <mike.joyce@shipt.com>
2024-06-15 09:43:44 -0400

    add more eslint snippets. seperate ts from js

--- lua/config/snippets.lua
+++ lua/config/snippets.lua
@@ -4,10 +4,22 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-local eslint_ignore_rules = {
-  s("eslint-ignore", { t("// eslint-ignore-next-line "), i(1, "rule"), t(" -- "), i(2, "explanation") }),
+local js_snippets = {
+  s("eslint-ignore", { t("// eslint-disable-next-line "), i(1, "rule"), t(" -- "), i(2, "explanation") }),
+  s("eslint-ignore-console", { t("// eslint-disable-next-line no-console -- "), i(1, "debugging") }),
 }
-ls.add_snippets("javascript", eslint_ignore_rules)
-ls.add_snippets("typescript", eslint_ignore_rules)
-ls.add_snippets("javascriptreact", eslint_ignore_rules)
-ls.add_snippets("typescriptreact", eslint_ignore_rules)
+local ts_snippets = {
+  s(
+    "eslint-ignore-any",
+    { t("// eslint-disable-next-line @typescript-eslint/no-explicit-any -- "), i(1, "explanation") }
+  ),
+}
+-- add global js_snippets
+ls.add_snippets("javascript", js_snippets)
+ls.add_snippets("typescript", js_snippets)
+ls.add_snippets("javascriptreact", js_snippets)
+ls.add_snippets("typescriptreact", js_snippets)
+
+-- add ts specific snippets
+ls.add_snippets("typescript", ts_snippets)
+ls.add_snippets("typescriptreact", ts_snippets)
