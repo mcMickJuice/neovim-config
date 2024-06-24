86b5601a0a434d27a20f7a512dff685791398325
Mike Joyce <mike.joyce@shipt.com>
2024-06-23 21:11:20 -0400

    add link to lualine docs

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
+    -- lualine docs - https://github.com/nvim-lualine/lualine.nvim
     -- show full filename in status bar - https://www.reddit.com/r/neovim/comments/16s1xky/comment/k27nf6g/
     local function filename()
       return "%F"
