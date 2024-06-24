4fc429177a3796bf2f092576bf1806bcf9778fb5
Mike Joyce <mike.joyce@shipt.com>
2024-06-23 20:53:20 -0400

    get full filename in status bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,18 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    require("lualine").setup {
+    -- show full filename in status bar - https://www.reddit.com/r/neovim/comments/16s1xky/comment/k27nf6g/
+    local function filename()
+      return "%F"
+    end
+    require("lualine").setup({
       options = {
         theme = "dracula",
       },
       sections = {
+        lualine_c = { filename },
         lualine_x = { "filetype" },
-      }
-    }
+      },
+    })
   end,
 }
