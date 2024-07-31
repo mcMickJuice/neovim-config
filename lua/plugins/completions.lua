232868a05dc73c021902418c5c3c0a4f531b2b06
Mike Joyce <mike.joyce@shipt.com>
2024-07-31 16:15:36 -0400

    add comment

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,102 +1,102 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-		-- from https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-		config = function()
-			-- load custom snippets
-			require("config.snippets")
-			local ls = require("luasnip")
-			-- see docs for this api - https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md
-			vim.keymap.set({ "i" }, "<C-K", function()
-				ls.expand()
-			end, { silent = true })
-			-- jump to next parameter in snippet
-			vim.keymap.set({ "i", "s" }, "<C-L>", function()
-				ls.jump(1)
-			end, { silent = true })
-			-- jump back to previous parameter in snippet
-			vim.keymap.set({ "i", "s" }, "<C-H>", function()
-				ls.jump(-1)
-			end, { silent = true })
-			-- I don't know what this does...
-			vim.keymap.set({ "i", "s" }, "<C-E>", function()
-				if ls.choice_active() then
-					ls.change_choice(1)
-				end
-			end, { silent = true })
-		end,
-	},
-	{
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    -- from https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+    config = function()
+      -- load custom snippets
+      require("config.snippets")
+      local ls = require("luasnip")
+      -- see docs for this api - https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md
+      vim.keymap.set({ "i" }, "<C-K", function()
+        ls.expand()
+      end, { silent = true })
+      -- jump to next parameter in snippet
+      vim.keymap.set({ "i", "s" }, "<C-L>", function()
+        ls.jump(1)
+      end, { silent = true })
+      -- jump back to previous parameter in snippet
+      vim.keymap.set({ "i", "s" }, "<C-H>", function()
+        ls.jump(-1)
+      end, { silent = true })
+      -- I don't know what this does...
+      vim.keymap.set({ "i", "s" }, "<C-E>", function()
+        if ls.choice_active() then
+          ls.change_choice(1)
+        end
+      end, { silent = true })
+    end,
+  },
+  {
+    -- lots of this is from teejs video - https://www.youtube.com/watch?v=_DnmphIwnjo&ab_channel=TJDeVries
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+      require("luasnip.loaders.from_vscode").lazy_load()
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+          { name = "nvim_lua" },
+          { name = "path" },
+          { name = "buffer",  keyword_length = 5 },
+        }),
+        experimental = {
+          ghost_text = true,
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- can also be a function to dynamically calculate max width such as
+            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+            menu = {
+              buffer = "[buf]",
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[api]",
+              path = "[path]",
+              luasnip = "[snip]",
+            },
 
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"onsails/lspkind.nvim",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-buffer",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local lspkind = require("lspkind")
-			require("luasnip.loaders.from_vscode").lazy_load()
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-					{ name = "nvim_lua" },
-					{ name = "path" },
-					{ name = "buffer", keyword_length = 5 },
-				}),
-				experimental = {
-					ghost_text = true,
-				},
-				formatting = {
-					format = lspkind.cmp_format({
-						mode = "symbol_text",
-						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-						-- can also be a function to dynamically calculate max width such as
-						-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-						menu = {
-							buffer = "[buf]",
-							nvim_lsp = "[LSP]",
-							nvim_lua = "[api]",
-							path = "[path]",
-							luasnip = "[snip]",
-						},
-
-						-- The function below will be called before any actual modifications from lspkind
-						-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-						-- before = function (entry, vim_item)
-						--   ...
-						--   return vim_item
-						-- end
-					}),
-				},
-			})
-		end,
-	},
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            -- before = function (entry, vim_item)
+            --   ...
+            --   return vim_item
+            -- end
+          }),
+        },
+      })
+    end,
+  },
 }
