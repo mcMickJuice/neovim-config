3e07ac1cbd11d3bb9453a123803736325d1d50ef
Mike Joyce <mike.joyce@shipt.com>
2024-08-23 06:26:12 -0400

    fuzzy refine list

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,6 +32,7 @@ return {
 			)
 
 			local telescope = require("telescope")
+			local actions = require("telescope.actions")
 			local lga_actions = require("telescope-live-grep-args.actions")
 
 			telescope.setup({
@@ -56,7 +57,7 @@ return {
 								["<C-k>"] = lga_actions.quote_prompt(),
 								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
 								-- freeze the current list and start a fuzzy search in the frozen list
-								-- ["<C-space>"] = actions.to_fuzzy_refine,
+								["<C-space>"] = actions.to_fuzzy_refine,
 							},
 						},
 					},
