6b7fe9b1bebece83d65f15fc2d6d555d016403ad
Mike Joyce <mike.joyce@shipt.com>
2024-08-23 06:20:10 -0400

    install live_grep_args, configure

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,20 +2,39 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+		},
 
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 			vim.keymap.set("n", "<leader>mp", builtin.man_pages, {})
 			vim.keymap.set("n", "<leader>rg", builtin.registers, {})
 			vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, {})
 
-			require("telescope").setup({
+      -- live_grep_args driven commands
+      -- https://github.com/nvim-telescope/telescope-live-grep-args.nvim?tab=readme-ov-file#shortcut-functions
+			local lga_shortcuts = require("telescope-live-grep-args.shortcuts")
+			vim.keymap.set("n", "<leader>fs", lga_shortcuts.grep_word_under_cursor)
+			vim.keymap.set(
+				"n",
+				"<leader>fg",
+				":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"
+			)
+
+			local telescope = require("telescope")
+			local lga_actions = require("telescope-live-grep-args.actions")
+
+			telescope.setup({
 				defaults = {
 					layout_strategy = "flex",
 					layout_config = {
@@ -29,7 +48,22 @@ return {
 						find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
 					},
 				},
+				extensions = {
+					live_grep_args = {
+            -- https://github.com/nvim-telescope/telescope-live-grep-args.nvim?tab=readme-ov-file#configuration
+						mappings = { -- extend mappings
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+								-- freeze the current list and start a fuzzy search in the frozen list
+								-- ["<C-space>"] = actions.to_fuzzy_refine,
+							},
+						},
+					},
+				},
 			})
+
+			telescope.load_extension("live_grep_args")
 		end,
 	},
 
