f3b2dee4c98692991701ac646f96574e51c732aa
Mike Joyce <mike.joyce@shipt.com>
2024-09-12 06:12:27 -0400

    harpoon delete entry

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -14,19 +14,39 @@ return {
 		-- basic telescope configuration
 		local conf = require("telescope.config").values
 		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
+			local finder = function()
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+				return require("telescope.finders").new_table({
+					results = file_paths,
+				})
 			end
 
 			require("telescope.pickers")
 				.new({}, {
 					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
+					finder = finder(),
+					-- smaller layout then normal telescope, without preview as I know  what's in the file
+					previewer = false,
+					layout_config = {
+						height = 0.6,
+						width = 0.6,
+					},
 					sorter = conf.generic_sorter({}),
+          -- how to delete entry from harpoon list
+          -- https://github.com/ThePrimeagen/harpoon/issues/499#issuecomment-2076462687
+					attach_mappings = function(prompt_bufnr, map)
+						map("i", "<C-d>", function()
+							local state = require("telescope.actions.state")
+							local selected_entry = state.get_selected_entry()
+							local current_picker = state.get_current_picker(prompt_bufnr)
+							table.remove(harpoon_files.items, selected_entry.index)
+							current_picker:refresh(finder())
+						end)
+						return true
+					end,
 				})
 				:find()
 		end
