95f557fb15389e94b977c6f0b9551d0a2d75e97e
Mike Joyce <mike.joyce@shipt.com>
2024-10-18 19:28:05 -0400

    Revert "comment out gitsigns to see if its the perf culprit"
    This reverts commit 30c9883c6b658013fd36e8d11a6d617def9c445c.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -13,11 +13,11 @@ return {
 	{
 		"f-person/git-blame.nvim",
 	},
-	-- {
-	-- 	"lewis6991/gitsigns.nvim",
-	-- 	config = function()
-	-- 		require("gitsigns").setup()
-	-- 		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-	-- 	end,
-	-- },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
 }
