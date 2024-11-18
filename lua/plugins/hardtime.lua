0e894920d936d37c2f13578838936b71c5d5ac20
Mike Joyce <mike.joyce@shipt.com>
2024-11-17 20:37:12 -0500

    add hardtime to enforce best practices

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,7 @@
+-- https://github.com/m4xshen/hardtime.nvim?tab=readme-ov-file
+-- plugin to prevent bad habits while using vim e.g. repeated j to navigate line at a time.
+return {
+   "m4xshen/hardtime.nvim",
+   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+   opts = {}
+}
