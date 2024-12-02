535f170771cb78fd710d4900750438bed275589a
Mike Joyce <mike.joyce@shipt.com>
2024-12-02 09:27:58 -0500

    enable format on save

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,12 +1,12 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- format on save
--- autocmd("BufWritePre", {
---   pattern = '',
---   callback = function()
---     vim.lsp.buf.format()
---   end,
--- })
+autocmd("BufWritePre", {
+  pattern = '',
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
 -- from https://neovim.io/doc/user/lua-guide.html#lua-guide-autocommand-create
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.highlight.on_yank() end,
