a9a6d6a2d438e6315ef3c6f584fd3250df08ce29
Mike Joyce <mike.joyce@shipt.com>
2024-12-05 05:27:05 -0500

    add format on save to none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,6 +2,7 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 		null_ls.setup({
 			sources = {
@@ -16,6 +17,20 @@ return {
 				-- requires pgformatter to be installed locally - https://formulae.brew.sh/formula/pgformatter
 				null_ls.builtins.formatting.pg_format,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+							vim.lsp.buf.formatting_sync()
+						end,
+					})
+				end
+			end,
 		})
 	end,
 }
