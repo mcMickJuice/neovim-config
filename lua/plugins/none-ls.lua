218334dc60182d46cb6010a7b20ea69d65338483
Mike Joyce <mike.joyce@shipt.com>
2024-12-05 05:06:40 -0500

    add back stylua formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,7 +5,7 @@ return {
 
 		null_ls.setup({
 			sources = {
-				-- null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.eslint_d.with({
 					condition = function(utils)
 						return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs" })
