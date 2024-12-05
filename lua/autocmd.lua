fae6d6307f2115211ddd57701b339163fc31be81
Mike Joyce <mike.joyce@shipt.com>
2024-12-05 05:26:55 -0500

    remove format on save

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,14 +1,3 @@
-local autocmd = vim.api.nvim_create_autocmd
-
--- format on save
-autocmd("BufWritePre", {
-	pattern = "",
-	callback = function()
-		if vim.bo.filetype ~= "oil" then
-			vim.lsp.buf.format()
-		end
-	end,
-})
 -- from https://neovim.io/doc/user/lua-guide.html#lua-guide-autocommand-create
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
