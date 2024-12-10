8f0b5e942062bbd41010ba4dccbf2e07c146a9bc
Mike Joyce <mike.joyce@shipt.com>
2024-12-10 05:52:14 -0500

    add telescope-fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
 				-- For major updates, this must be adjusted manually.
 				version = "^1.0.0",
 			},
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		},
 
 		config = function()
