8436598ac40b22b2a30f7ffdce85a9f10ebe2d94
Mike Joyce <mike.joyce@shipt.com>
2024-12-18 20:46:42 -0500

    fix trouble, add keybindings

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,10 +1,18 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	cmd = "Trouble",
 	opts = {},
-	config = function()
-		vim.keymap.set("n", "<leader>xx", function()
-			require("trouble").toggle()
-		end)
-	end,
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+	},
 }
