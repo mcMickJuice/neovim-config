7bbaa9d3282d34d4b1e16ad9f9b71c496fa466e5
Mike Joyce <mike.joyce@shipt.com>
2025-09-02 06:33:53 -0400

    add copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,1 +1,13 @@
-return { "github/copilot.vim" }
+return {
+	"github/copilot.vim",
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		build = "make tiktoken",
+		opts = {
+			-- See Configuration section for options
+		},
+	},
+}
