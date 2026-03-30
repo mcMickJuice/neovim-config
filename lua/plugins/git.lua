d7e73daafa525b2812b66108fad3faf8a0dd3ced
Mike Joyce <mikejoyce19@gmail.com>
2026-03-30 06:00:40 -0400

    disable lg in nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -20,23 +20,24 @@ return {
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 		end,
 	},
-	{
-		"folke/snacks.nvim",
-		config = function()
-			local Snacks = require("snacks")
-			vim.keymap.set("n", "<leader>lg", function()
-				Snacks.lazygit.open()
-			end)
-			vim.keymap.set("n", "<leader>lf", function()
-				Snacks.lazygit.log_file()
-			end)
-		end,
-		opts = {
-			lazygit = {
-				-- your lazygit configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			},
-		},
-	},
+	-- I'm controlling lazy git with tmux so that it appears over the entire screen, not just in the current buffer or pane. This is controlled in .tmux.conf
+	-- {
+	-- 	"folke/snacks.nvim",
+	-- 	config = function()
+	-- 		local Snacks = require("snacks")
+	-- 		vim.keymap.set("n", "<leader>lg", function()
+	-- 			Snacks.lazygit.open()
+	-- 		end)
+	-- 		vim.keymap.set("n", "<leader>lf", function()
+	-- 			Snacks.lazygit.log_file()
+	-- 		end)
+	-- 	end,
+	-- 	opts = {
+	-- 		lazygit = {
+	-- 			-- your lazygit configuration comes here
+	-- 			-- or leave it empty to use the default settings
+	-- 			-- refer to the configuration section below
+	-- 		},
+	-- 	},
+	-- },
 }
